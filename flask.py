9462e9a4d2f39dfe96e77cf14e0ff0d76fa99018
Michele <micheleddd@gmail.com>
2023-04-15 14:29:45 +0200

    correction

--- flask.py
+++ flask.py
@@ -1,12 +1,9 @@
 import requests, flask
-
+from flask import render_template 
 app = flask.Flask(__name__)
     
 
-    
-  
-    
-
-@app.route("/"):
+@app.route("/")
+def index():
     return render_template("index.html"title='Welcome', username='Mich')
 
