9efdc300904319216bedaa190329545f531af8cf
Michele <micheleddd@gmail.com>
2023-04-15 14:57:40 +0200

    test1

--- conftest.py
+++ conftest.py
@@ -1,8 +1,9 @@
-from app import create_app
-import pytest
 
 
-@pytest.fixture
-def app():
-    app = create_app()
-    return app
+from app import app # Flask instance of the API
+
+def test_index_route():
+    response = app.test_client().get('/')
+
+    assert response.status_code == 200
+    assert response.data.decode('utf-8') == 'Testing, Flask!'
